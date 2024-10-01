f1aa750c1605e09df9be78efb58d2be8e4b6af39
sharwin neema <you@example.com>
2024-10-01 15:34:26 +0530

    added fragment to info and its route

--- app/src/main/java/androidsamples/java/dicegames/InfoFragment.java
+++ app/src/main/java/androidsamples/java/dicegames/InfoFragment.java
@@ -1,35 +1,26 @@
 package androidsamples.java.dicegames;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-
-/**
- * A {@link Fragment} that implements the Game Play screen.
- */
-public class GamesFragment extends Fragment {
+public class InfoFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_games, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.infoButton).setOnClickListener(v -> {
-            NavDirections action = GamesFragmentDirections.actionGamesFragmentToInfoFragment();
-            Navigation.findNavController(view).navigate(action);
-        });
+        TextView rulesTextView = view.findViewById(R.id.rulesTextView);
+        rulesTextView.setText(getString(R.string.rules_text)); // Add a rules text in strings.xml
     }
 }
